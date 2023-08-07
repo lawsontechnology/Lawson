efbdaecc503ca78a2e1dd14d87e6a153d035d19f
lawsontechnology <fatiulawal85@gmail.com>
2023-08-07 16:35:04 +0100

    message

---
+++ Program.cs
@@ -0,0 +1,234 @@
+﻿using System;
+
+namespace Leaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.Write("you are to assign two value :  ");
+            // Console.WriteLine(" input first number");
+            // int num1 = int.Parse(Console.ReadLine());
+            // Console.WriteLine(" input second number");
+            // int num2 = int.Parse(Console.ReadLine());
+            //  int num3 ;
+            //   num3 = num2;
+            //   num2 = num1;
+            //   num1 = num3;
+            // Console.WriteLine($" the swapping result is {num1}");
+            // Console.WriteLine($" the swapping result is {num2}");
+
+                  // no 2
+            // Console.WriteLine(" input any number of your choice");
+            // int num = int.Parse(Console.ReadLine());
+            // if(num % 2 == 0)
+            // {
+            //     Console.WriteLine($" {num} is an even number");
+            // }
+            // else
+            // {
+            //     Console.WriteLine($" {num} is a odd number ");
+            // }
+                // no 3
+                // Console.WriteLine(" input a value that can be divisible by 5 & 7");
+                // int value = int.Parse(Console.ReadLine());
+                // if(value % 5 == 0 && value % 7 == 0)
+                // {
+                //     Console.WriteLine($" {value} is divisible by 5 & 7");
+                // }
+                // else
+                // {
+                //     Console.WriteLine($"{value} is not divisible by 5 & 7");
+                // }
+                  
+                  // no 4
+                //   Console.WriteLine(" input a three digit number");
+                //   int number = int.Parse(Console.ReadLine());
+                //   int first;
+                //   int second;
+                  
+                //    first = number % 10;
+                //     Console.WriteLine($" the first digit is  {first}");
+                //     number /= 10;
+                //     second = number % 10;
+                //     Console.WriteLine($" the second digit is {second}");
+                //        number /= 10;
+                    
+                //       if(number == 7)
+                //       {
+                //         Console.WriteLine($" the third number is {number}");
+                //       }
+                //       else{
+                //         Console.WriteLine(" the third number is not 7 ");
+                //       }
+                    
+                  // no 5
+                  // Console.WriteLine(" we are to calculate the area of trapeziod  ");
+                  // Console.WriteLine(" input side A");
+                  // double sideA = double.Parse(Console.ReadLine());
+                  // Console.WriteLine(" input side B");
+                  // double sideB = double.Parse(Console.ReadLine());
+                  // Console.WriteLine(" input the height");
+                  // double height = double.Parse(Console.ReadLine());
+                  //  Console.WriteLine("area :"+ 1/2 * sideA + sideB* height);
+                    // no 6
+                   // Console.WriteLine("pls kindly enter the side value  you choose to use:");
+                   // double side = double.Parse(Console.ReadLine());
+                   // Console. WriteLine("pls kindly enter the height value you choose to use :");
+                   // double height = double.Parse(Console.ReadLine());
+                   // double sum = side + height;
+                   // Console.WriteLine("the answer is : " + sum );
+
+                    // no 7
+                     // Console.WriteLine("enter your weight ");
+                   // double weightmoon = double.Parse(Console.ReadLine());
+
+                   // double weightearth = weightmoon * 17/100;
+                   // Console.WriteLine(" the result is : " + weightearth);
+                      // no 8
+                   // Console.WriteLine("pls kindly Enter the first number ");
+                   // int first = int.Parse(Console.ReadLine());
+                   // Console.WriteLine("pls kindly enter the second number ");
+                   // int second = int.Parse(Console.ReadLine());
+                   // Console.WriteLine("pls kindly enter the third number ");
+                   // int third =  int.parse(Console.ReadLine());
+                   // int sum = first + second + third;
+                   // Console.WriteLine("the sum of all the int is : "+ sum);
+          
+                   // no 10
+
+                   // Console.WriteLine(" The name of company : Code Learners Hub group of company");
+                   //Console.WriteLine("address :odo eran bus-stop");
+                   // Console.WriteLine("Phone Number : +23491230000");
+                   // Console.WriteLine("Fax Number : 080codeLearnershub");
+                   // Console.WriteLine("website : www.Codeleanershub.com");
+                   // Console.Write("manger Name :  ");
+                   //  Console.WriteLine("Fatiu");
+                   // Console.Write("manger surname :  ");
+                   // Console.WriteLine("Lawal");
+                   // Console.Write("manger phone number :  ");
+                   // Console.WriteLine("09014146854");
+
+                  //  Console.WriteLine("we are to look for the greatest of all the 5 digit");
+                  //  Console.WriteLine("Enter first number");
+                  //  int first = int.Parse(Console.ReadLine());
+                  //  Console.WriteLine("Enter second number");
+                  //  int second = int.Parse(Console.ReadLine());
+                  //  Console.WriteLine("Enter third number");
+                  //  int third = int.Parse(Console.ReadLine());
+                  //  Console.WriteLine("Enter fourth number");
+                  //  int fourth = int.Parse(Console.ReadLine());
+                  //  Console.WriteLine("Enter fifth number");
+                  //  int fifth = int.Parse(Console.ReadLine());
+              
+                   
+
+                  //  if(first > second && first > third && first > fourth && first > fifth)
+                  // {
+                    
+                  //  Console.WriteLine($" the higher num is { first}");
+                  // }
+                  // else if(second > first && second > third && second > fourth && second > fifth)
+                  // {
+                  //    Console.WriteLine($" the higher num is { second}");
+                  // }
+                  // else if(third > first && third > second && third > fourth && third > fifth)
+                  // {
+                  //    Console.WriteLine($" the higher num is { third}");
+                  // }
+                  // else if(fourth > first && fourth > second && fourth > third && fourth > fifth)
+                  // {
+                  //    Console.WriteLine($" the higher num is { fourth}");
+                  // }
+                  // else if( fifth > first && fifth > second && fifth > third && fifth > fourth)
+                  // {
+                  //    Console.WriteLine($" the higher num is { fifth}");
+                  // }
+                  // else 
+                  // {
+                  //   Console.WriteLine(" check the number again");
+                  // }
+
+                  // no 12
+                  
+                    
+                  // no 13
+                //   Console.WriteLine("Enter first number");
+                //   int first = int.Parse(Console.ReadLine());
+                //  Console.WriteLine("Enter second number");
+                //  int second = int.Parse(Console.ReadLine());
+              
+                //  int greaternumber = second;
+
+                //  if(first > second)
+                //  {
+                //    greaternumber = first;
+                //  }
+                //  Console.WriteLine($" the higher num is { greaternumber}");
+
+                       ///// loop //////
+              //  Console.WriteLine("loop ");
+              //  Console.Write("from 1 to what did you wish to print :");
+              //  int wish = int.Parse(Console.ReadLine());
+
+              //  for (int i = 1; i <= wish; i++)
+              //  {
+              //    Console.WriteLine(i);
+              //  }
+
+
+            //   Console.WriteLine(" input number to check if is divisible by 3 & 7");
+            //   int input = int.Parse(Console.ReadLine());
+            //   bool check = true;
+
+            //  while (check)
+            //  {
+            //     if(input % 3==0 && input % 7==0)
+            //     {
+            //       Console.WriteLine($"{input} is divisible by 3 and 7");
+            //     }
+            //     else
+            //     {
+            //      Console.WriteLine($"{input} is not divisble by 3 and 7");
+            //     }
+            //       check =false;
+            //  }
+
+                
+                // Console.WriteLine("to check for the max & min number");
+                // Console.WriteLine("how many number did you wish to check");
+                // int num = int.Parse(Console.ReadLine());
+
+                // int[] number = new int[num];
+                // int max = int32.MaxValue(0);
+                // int min = int32.MinValue(0);
+                // for (int i = 0; i <nuber.length; i++)
+                // {
+                //   Console.WriteLine("input the value");
+                //   number[i]=int.Parse(Console.ReadLine());
+
+                //   if()
+                // }
+
+
+     
+              //   string[] lucky = { "Hearts", "Diamonds", "Clubs", "Spades" };
+              //   string[] won = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+
+              //    foreach (string lucky in lucky)
+              //  {
+              //    foreach (string won in won)
+              //    {
+              //     string play = won + " of " + lucky;
+              //      Console.WriteLine(play);
+              //    }
+              //  }
+  
+  
+
+
+
+
+        }
+    }
+}
